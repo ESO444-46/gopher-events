a94b79642353f4feaa9577c8a419668c00855413
Ubuntu <ubuntu@ip-172-31-33-44.us-east-2.compute.internal>
2026-07-24 04:43:17 +0000

    Add CORS config for gopherevent.com production domain

--- backend/src/app.js
+++ backend/src/app.js
@@ -3,10 +3,16 @@ const app = express()
 require('dotenv').config()
 
 const cors = require("cors")
+
 const authRoutes = require('./routes/auth.routes')
 const eventRoutes = require('./routes/event.routes')
 
-app.use(cors())
+const corsOptions = {
+	origin : "https://gopherevent.com",
+	credentials: true,
+};
+
+app.use(cors(corsOptions));
 app.use(express.json());
 
 app.use('/auth', authRoutes)
